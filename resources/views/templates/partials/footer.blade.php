dcb7b662cf6393735e38e394ae727656062caa9e
Thomas | Dark Roast <thomas@darkroast.nu>
2019-08-12 13:33:13 +0200

    AdminLTE template for footer

--- resources/views/templates/partials/footer.blade.php
+++ resources/views/templates/partials/footer.blade.php
@@ -1,8 +1,8 @@
  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; {{ date('Y')}} <a href="#">Company</a>.</strong> All rights reserved.
-  </footer>
+ 	<!-- To the right -->
+ 	<div class="pull-right hidden-xs">
+ 		<i class="fa fa-bug"></i> Problemen, vragen of opmerkingen over de werkgroepensite? <a href="mailto:webgroep@iewan.nl">Mail de webgroep</a>
+ 	</div>
+ 	<!-- Default to the left -->
+ 	<strong><a href="https://iewan.nl">Iewan</a> - {{ date('Y')}}</strong>
+ </footer>

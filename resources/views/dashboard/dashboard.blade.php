2a776361febadfb77aac17a871ff75dd5bb93b52
wenkunst <info@wenkunst.nl>
2019-08-16 09:45:04 +0200

    Dashboard titlebar recent uploaded files improved

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -26,23 +26,21 @@
 			])
 
 			<div class="box">
+                
 				<div class="box-header">
 					<h3 class="box-title">Recent geuploade bestanden</h3>
 
-					<div class="box-tools">
-						<div class="row">
-							<!-- select -->
 
+                    <div class="box-tools">
+                        <div class="input-group input-group-sm hidden-xs" style="width: 150px;">
+                            <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
 
-							<div class="input-group input-group-sm hidden-xs col-xs-5">
-								<input type="text" name="table_search" class="form-control" placeholder="Search">
+                            <div class="input-group-btn">
+                                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </div>
 
-								<div class="input-group-btn">
-									<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body table-responsive no-padding">

673e1f6b09caea6fc3e0ebee3b8f851e79630140
Thomas | Dark Roast <thomas@darkroast.nu>
2019-08-27 22:24:07 +0200

    bit of template cleaned up at the binder

---
+++ resources/views/binder/partials/binder-list.blade.php
@@ -0,0 +1,59 @@
+			<div class="box">
+				<div class="box-header">
+					<h3 class="box-title">Klapper</h3>
+
+					<div class="box-tools">
+						<div class="input-group input-group-sm hidden-xs" style="width: 150px;">
+							<input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+							<div class="input-group-btn">
+								<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body table-responsive no-padding">
+					<table class="table table-hover">
+						<tr>
+
+							<th style="width: 240px">Aanmelding</th>
+							<th>Naam</th>
+							<th>Naam partner</th>
+							<th>Woningvoorkeur (1)</th>
+							<th>woningvoorkeur (2)</th>
+							<th style="width: 40px"></th>
+							<th style="width: 40px"></th>
+							<th style="width: 40px"></th>
+
+
+
+						</tr>
+
+						<tr>
+							<td>2 aug '19</td>
+							<td><a href="#">Thomas Mennen</a></td>
+							<td>Ani Öhman</td>
+							<td>Eenpersoons</td>
+							<td>Gezinswoning</td>
+							<td></td>
+							<td><button class="btn btn-default" title="Bewerken"><i class="fa fa-pencil"></i><span class="sr-only">Bewerken</span></button></td>
+							<td><button class="btn btn-default" title="Verwijderen"><i class="fa fa-trash"></i><span class="sr-only">Verwijderen</span></button></td>
+						</tr>
+
+					</table>
+				</div>
+				<div class="box-footer clearfix">
+					<div class="box-tools pull-right">
+						<div class="input-group input-group-sm hidden-xs" style="width: 150px;">
+							<input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+							<div class="input-group-btn">
+								<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.box-body -->
+			</div>
+			<!-- /.box -->
